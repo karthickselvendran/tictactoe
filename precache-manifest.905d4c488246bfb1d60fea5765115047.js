1bd53949451c0b2f6a4134145fc93be191d7dae0
Karthick S <selvendrankarthick@gmail.com>
2020-02-18 00:09:24 +0530

    Updates

---
+++ precache-manifest.905d4c488246bfb1d60fea5765115047.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a805df341e8411743c3aaa03bcddafe",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "330a9b2819f8b3fa140f",
+    "url": "/tictactoe/static/css/2.df49e3ca.chunk.css"
+  },
+  {
+    "revision": "48dcab91efbf8f29cba4",
+    "url": "/tictactoe/static/css/main.a78bb610.chunk.css"
+  },
+  {
+    "revision": "330a9b2819f8b3fa140f",
+    "url": "/tictactoe/static/js/2.219ebcc5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tictactoe/static/js/2.219ebcc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48dcab91efbf8f29cba4",
+    "url": "/tictactoe/static/js/main.474151b1.chunk.js"
+  },
+  {
+    "revision": "f9796d282907b93526d6",
+    "url": "/tictactoe/static/js/runtime-main.e11d0aff.js"
+  }
+]);
